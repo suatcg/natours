a2046fa0db953a2fdc902be8c0118e433b68e9bc
suatcg <suat-can_24@hotmail.com>
2022-11-23 15:23:11 +0300

    error-handling

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -18,10 +20,10 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 // custom middleware
-app.use((req, res, next) => {
-  console.log('Hello from middleware!');
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log('Hello from middleware!');
+//   next();
+// });
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -34,6 +36,25 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-// 4) START SERVER
+// Handle the uncontained request (all works for all http methods such as GET, POST... etc)
+// * start indicates all URL that coming from request
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server`,
+  // });
+
+  // const err = new Error(`Can't find ${req.originalUrl} on this server`);
+  // err.status = 'fail';
+  // err.statusCode = 404;
+
+  // When we pass an argument into next func. express automatically receive an error so that will stops all other middleware and send the error that passed into next func as an argument.
+  // next(err);
+
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+});
+
+// Global Error Handler Middleware, Express understands this is an error handler middleware due to middleware has 4 arguments.
+app.use(globalErrorHandler);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+// Uncaught Exceptions
+process.on('uncaughtException', (err) => {
+  console.log('ERROR: ', err.name, err.message);
+  console.log('UNCAUGHT EXEPTION: 🔥 Shutting down...');
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
@@ -15,11 +22,13 @@ mongoose
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
+    useUnifiedTopology: true,
   })
   .then(() => {
     // console.log(con.connections);
     console.log('DB connection successful!');
   });
+// .catch((err) => console.error('ERROR'));
 
 // Testind model Tour
 // const testTour = new Tour({
@@ -43,7 +52,20 @@ mongoose
 // console.log(process.env);
 
 const port = process.env.PORT;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log(`App running on port ${port}...`);
+  console.log('Enviorment :', process.env.NODE_ENV);
+});
+
+// Resolve Unhandler promise rejection
+process.on('unhandledRejection', (err) => {
+  console.log('ERROR: ', err.name);
+  // console.log(err.message);
+
+  console.log('UNHANDLER REJECTION: 🔥 Shutting down...');
+  // 0 -> success ,1 -> uncaught exception
+  server.close(() => {
+    process.exit(1);
+  });
 });
