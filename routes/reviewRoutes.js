fdb48d3d13c64f00658ebeb0643d08932976ce03
suatcg <suat-can_24@hotmail.com>
2022-12-13 00:54:00 +0300

    Indexes-geospitial-aggregation

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -4,6 +4,8 @@ const authController = require('../controllers/authController');
 
 const router = express.Router({ mergeParams: true });
 
+router.use(authController.protect);
+
 // POST /tours/3dfs4g/reviews
 // GET /tours/3dfs4g/reviews
 // POST /reviews
@@ -11,9 +13,21 @@ router
   .route('/')
   .get(reviewController.getAllReviews)
   .post(
-    authController.protect,
     authController.restrictTo('user'),
+    reviewController.setTourUserIds,
     reviewController.createReview
+  );
+
+router
+  .route('/:id')
+  .get(reviewController.getReview)
+  .patch(
+    authController.restrictTo('user', 'admin'),
+    reviewController.updateReview
+  )
+  .delete(
+    authController.restrictTo('user', 'admin'),
+    reviewController.deleteReview
   );
 
 module.exports = router;
